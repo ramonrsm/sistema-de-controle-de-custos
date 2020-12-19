910ae10cec1b62aa226eba1e7669637dc2edf5f4
Ramon Moura <ramonmourarsm@outlook.com>
2020-12-19 20:50:14 -0300

    Backend: Adição do express

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,7 @@
-console.log('Teste Typescript');
+import express from "express";
+
+const app = express();
+
+const porta = 4000;
+
+app.listen(porta, () => console.log(`Servidor rodando em http://localhost:${porta}`));
