088107762a89cd0089771736ba1230582b2254f5
Ramon Moura <ramonmourarsm@outlook.com>
2020-12-23 22:40:26 -0300

    Backend: Adição do middleware helmet

--- backend/src/config/middleware.ts
+++ backend/src/config/middleware.ts
@@ -1,6 +1,8 @@
 import { Express, json } from 'express';
+import helmet from 'helmet';
 
 export default (app: Express) => {
+    app.use(helmet());
     app.use(json());
     console.log('Middlewares importados.');    
 }
