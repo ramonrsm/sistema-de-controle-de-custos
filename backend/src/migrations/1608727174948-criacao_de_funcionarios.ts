2809bd6b1b17d024bf4b5315642fda16a9f0e9f5
Ramon Moura <ramonmourarsm@outlook.com>
2020-12-27 14:16:05 -0300

    Backend: Refatoração na migration criacaoDeFuncionarios

--- backend/src/migrations/1608727174948-criacao_de_funcionarios.ts
+++ backend/src/migrations/1608727174948-criacao_de_funcionarios.ts
@@ -3,14 +3,31 @@ import {MigrationInterface, QueryRunner, Table} from "typeorm";
 export class criacaoDeFuncionarios1608727174948 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(new Table({
+            name: "departamentos",
+            columns: [
+                {
+                    name: "id",
+                    type: "varchar",
+                    isPrimary: true,
+                    generationStrategy: 'uuid'
+                },
+                {
+                    name: "nome",
+                    type: "varchar(100)",
+                }
+            ]
+        }), true);
+
         await queryRunner.createTable(new Table({
             name: "funcionarios",
             columns: [
                 {
                     name: "id",
-                    type: "int",
+                    type: "varchar",
                     isPrimary: true,
-                    isGenerated: true
+                    generationStrategy: 'uuid'
                 },
                 {
                     name: "nome",
@@ -18,10 +35,50 @@ export class criacaoDeFuncionarios1608727174948 implements MigrationInterface {
                 }
             ]
         }), true);
+
+        await queryRunner.createTable(new Table({
+            name: "funcionario_departamento",
+            columns: [
+                {
+                    name: "id",
+                    type: "varchar",
+                    isPrimary: true,
+                    generationStrategy: 'uuid'
+                },
+                {
+                    name: "funcionario_id",
+                    type: "varchar"
+                },
+                {
+                    name: "departamento_id",
+                    type: "varchar"
+                }
+            ],
+            foreignKeys: [
+                {
+                    name: 'FuncionarioDepartamento',
+                    columnNames: ['funcionario_id'],
+                    referencedTableName: 'funcionarios',
+                    referencedColumnNames: ['id'],
+                    onUpdate: 'CASCADE',
+                    onDelete: 'CASCADE'
+                },
+                {
+                    name: 'DepartamentoFuncionario',
+                    columnNames: ['departamento_id'],
+                    referencedTableName: 'departamentos',
+                    referencedColumnNames: ['id'],
+                    onUpdate: 'CASCADE',
+                    onDelete: 'CASCADE'
+                }
+            ]
+        }), true);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('funcionario_departamento', true);
         await queryRunner.dropTable('funcionarios', true);
+        await queryRunner.dropTable('departamentos', true);
     }
 
 }
