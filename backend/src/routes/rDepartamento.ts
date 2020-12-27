481b85b9738d85086baa8a701122ab4c71460cdd
Ramon Moura <ramonmourarsm@outlook.com>
2020-12-27 19:06:05 -0300

    Backend: Correção na chamada da função de validação de buscar departamentos

--- backend/src/routes/rDepartamento.ts
+++ backend/src/routes/rDepartamento.ts
@@ -5,6 +5,6 @@ import cDepartamento from '../controllers/cDepartamento';
 const departamentosRoutes = Router();
 
 departamentosRoutes.post('/departamentos', vDepartamento.validarCadastroDepartamento, cDepartamento.criarDepartamento);
-departamentosRoutes.get('/departamentos', vDepartamento.buscarDepartamentos, cDepartamento.buscarDepartamentos);
+departamentosRoutes.get('/departamentos', vDepartamento.validarBuscarDepartamentos, cDepartamento.buscarDepartamentos);
 
 export default departamentosRoutes;
