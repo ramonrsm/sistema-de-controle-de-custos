1809c0a72a7d85d95e953207241e8fe3d688b827
Ramon Moura <ramonmourarsm@outlook.com>
2020-12-29 06:11:45 -0300

    Backend: Adicionando cors

--- backend/src/config/middleware.ts
+++ backend/src/config/middleware.ts
@@ -1,8 +1,10 @@
 import { Express, json } from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 
 export default (app: Express) => {
     app.use(helmet());
     app.use(json());
+    app.use(cors());
     console.log('Middlewares importados.');    
 }
