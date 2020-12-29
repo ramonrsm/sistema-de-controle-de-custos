51c81a170040272a44dd9622dbc15dae28aa1328
Ramon Moura <ramonmourarsm@outlook.com>
2020-12-29 04:15:16 -0300

    Frontend: Ajuste na linguagem da página e no css geral

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'jquery';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
